4fcdb29ac4fa277646ee1ca56b6bd99b73618609
LuisGOAquino <luisoliveiraaquino@gmail.com>
2025-04-27 15:13:01 -0300

    Question 2: Guardar dado

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,8 @@ def rolar_dados(n):
         sorteado= randint(1,6)
         l.append(sorteado)
     return l
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dado=dados_rolados[dado_para_guardar]
+    dados_no_estoque.append(dado)
+    del(dados_rolados[dado_para_guardar])
+    return [dados_rolados, dados_no_estoque]
