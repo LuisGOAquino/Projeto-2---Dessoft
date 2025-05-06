645c2d58a3452f6a54224518d6790778d6dc7ce0
LuisGOAquino <luisoliveiraaquino@gmail.com>
2025-05-06 14:40:31 -0300

    Parte 2 - Questao 13

--- programa.py
+++ programa.py
@@ -31,47 +31,53 @@ while rodada<12:
     print(f'Dados guardados: {guardados}')
     c = 0
     print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
-    ação = int(input('>'))
-    while ação != 0:
-        if ação == 1:
+    ação = (input('>'))
+    while ação != '0':
+        if ação == '1':
             print('Digite o índice do dado a ser guardado (0 a 4):')
             ind = int(input('>'))
-            while ind > 4 or ind < 0:
+            while ind > len(dados) - 1 or ind < 0:
                 print('Opção inválida. Tente novamente.')
                 print('Digite o índice do dado a ser guardado (0 a 4):')
                 ind = int(input('>'))
             lista = guardar_dado(dados,guardados,ind)
             dados = lista[0]
             guardados = lista[1]
-        if ação == 2:
+        while guardados == [] and ação == '2':
+            print('Opção inválida. Tente novamente.')
+            ação=(input('>'))  
+        if ação == '2':
             print('Digite o índice do dado a ser removido (0 a 4):')
             ind = int(input('>'))
-            while ind > 4 or ind < 0:
+            while ind > len(guardados) - 1 or ind < 0:
                 print('Opção inválida. Tente novamente.')
                 print('Digite o índice do dado a ser removido (0 a 4):')
                 ind = int(input('>'))
             lista = remover_dado(dados,guardados,ind)
             dados = lista[0]
             guardados = lista[1]
-        if ação == 3:
-            if c >= 3:
+        if ação == '3':
+            if c == 2:
                 print('Você já usou todas as rerrolagens.')
-                print(f'Dados rolados: {dados}')
-                print(f'Dados guardados: {guardados}')
-                print(('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:'))
-                ação = int(input('>'))
-            numero = 5-len(guardados)
-            dados = rolar_dados(numero)
+            else:
+                numero = 5-len(guardados)
+                dados = rolar_dados(numero)
+                c+=1
             print(f'Dados rolados: {dados}')
             print(f'Dados guardados: {guardados}')
-            c+=1
-        if ação == 4:
+            print(('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:'))
+            ação = (input('>'))
+        if ação == '4':
             imprime_cartela(cartela)
             print(f'Dados rolados: {dados}')
             print(f'Dados guardados: {guardados}')
+        while ação not in ['0','1','2','3','4']:
+            print('Opção inválida. Tente novamente.')
+            ação = (input('>'))
+        print(f'Dados rolados: {dados}')
+        print(f'Dados guardados: {guardados}')      
         print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
-        ação = int(input('>'))
+        ação = (input('>'))
     print('Digite a combinação desejada:')
     i = input('>')
-    fazer a parte de colocar tudo na cartela
     rodada +=1
